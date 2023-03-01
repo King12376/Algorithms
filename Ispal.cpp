8b1c16a02f0312961af2d84e16dd0334dfcaf83e
Majahenkhosi <411021340@gms.ndhu.edu.tw>
2023-03-02 03:43:03 +0800

    Ispal

---
+++ Ispal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+bool Ispalindrome (string s) {
+  for (int i = 0; i < s.length() / 2; i++)  //iterate through the word
+    if (s[i] != s[s.length() - i -1 ])      //check from left and right if the letters are the same in the word
+    return false;
+  
+  return true;
+
+}
+
+int main () {
+  string name;
+  name = "abcbca";
+
+ 
+  if (Ispalindrome(name))     //thats if the condition is true
+  cout << name  << "    is a palindrome"<<endl;
+  else
+  cout << name << " is not a palindrome" <<endl;
+
+}
